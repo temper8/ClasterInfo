7073caad5aa7e751b1d4ccc74a2620e4152764d2
Alex <temper8@gmail.com>
2024-05-30 01:25:48 +0300

    Update test_connection.py

--- test_connection.py
+++ test_connection.py
@@ -2,23 +2,25 @@ import paramiko
 import json
 import sys
 
-fname = 'site.json'
-try:
-    f = open(fname, 'rb')
-except OSError:
-    print("Could not open/read file:", fname)
-    sys.exit()
-with f:
-    data = json.load(f)
-    host = data['host'] 
-    user = data['user'] 
-    port = 22
+def get_info(fname:str):
+    try:
+        f = open(fname, 'rb')
+    except OSError:
+        print("Could not open/read file:", fname)
+        sys.exit()
+    with f:
+        data = json.load(f)
+    return data['host'], data['user']  
 
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=host, username=user, port=port)
-    stdin, stdout, stderr = client.exec_command('ls -l')
-    data = stdout.read() + stderr.read()
-    client.close()
+host, user = get_info('site.json')
 
-    print(data.decode("utf-8"))
+port = 22
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(hostname=host, username=user, port=port)
+stdin, stdout, stderr = client.exec_command('ls -l')
+data = stdout.read() + stderr.read()
+client.close()
+
+print(data.decode("utf-8"))
