7e0a6bf21164d1843694607d16edc7b028c6be3e
Alex <temper8@gmail.com>
2024-05-31 03:50:38 +0300

    Update tlg_send.py

--- tlg_send.py
+++ tlg_send.py
@@ -59,10 +59,9 @@ def get_remote_sinfo(host:str, user:str):
 #host, user = get_secrets('config.json')
 info = load_sinfo('head1_sinfo.log')
 print(info)
-info = info.replace('up 60-00:00:0', '')
+info = info.replace('cascade*     up 60-00:00:0', '')
 info = info.replace('TIMELIMIT', '')
-info = info.replace('PARTITION', 'PART')
-info = info.replace('    ', '')
+info = info.replace('PARTITION', ' ')
 print(info)
 h = hashlib.md5(info.encode('utf-8')).hexdigest()
 if compare_hash(h):
